27af2eea7f45e2bf482d1d064026a2f3253148cd
Mohsen Vakilian <reprogrammer@gmail.com>
2012-06-15 18:58:22 -0500

    Implemented the move to immediate superclass action.
    This commit belongs to issue #1.

--- edu.illinois.compositerefactorings.extractsuperclass/src/edu/illinois/compositerefactorings/extractsuperclass/CompositeRefactoringsQuickAssistProcessor.java
+++ edu.illinois.compositerefactorings.extractsuperclass/src/edu/illinois/compositerefactorings/extractsuperclass/CompositeRefactoringsQuickAssistProcessor.java
@@ -8,6 +8,10 @@ import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.core.dom.SimpleType;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
 import org.eclipse.jdt.core.dom.rewrite.ListRewrite;
@@ -28,7 +32,8 @@ public class CompositeRefactoringsQuickAssistProcessor implements IQuickAssistPr
 	public boolean hasAssists(IInvocationContext context) throws CoreException {
 		ASTNode coveringNode= context.getCoveringNode();
 		if (coveringNode != null) {
-			return getCreateNewSuperclassProposal(context, coveringNode, false, null);
+			return getCreateNewSuperclassProposal(context, coveringNode, false, null) ||
+					getMoveToImmediateSuperclassProposal(context, coveringNode, false, null);
 		}
 		return false;
 	}
@@ -39,6 +44,7 @@ public class CompositeRefactoringsQuickAssistProcessor implements IQuickAssistPr
 		if (coveringNode != null) {
 			ArrayList<ICommandAccess> resultingCollections= new ArrayList<ICommandAccess>();
 			getCreateNewSuperclassProposal(context, coveringNode, false, resultingCollections);
+			getMoveToImmediateSuperclassProposal(context, coveringNode, false, resultingCollections);
 			return resultingCollections.toArray(new IJavaCompletionProposal[resultingCollections.size()]);
 		}
 		return null;
@@ -67,6 +73,38 @@ public class CompositeRefactoringsQuickAssistProcessor implements IQuickAssistPr
 		listRewrite.insertLast(newSuperclass, null);
 		rewrite.set(newSuperclass, TypeDeclaration.SUPERCLASS_TYPE_PROPERTY, coveringTypeDeclarationASTNode.getStructuralProperty(TypeDeclaration.SUPERCLASS_TYPE_PROPERTY), null);
 		rewrite.set(coveringTypeDeclarationASTNode, TypeDeclaration.SUPERCLASS_TYPE_PROPERTY, newSuperclass.getName(), null);
+		proposals.add(proposal);
+		return true;
+	}
+
+	private static boolean getMoveToImmediateSuperclassProposal(IInvocationContext context, ASTNode coveringNode, boolean problemsAtLocation, Collection<ICommandAccess> proposals)
+			throws CoreException {
+		if (!(coveringNode instanceof SimpleName) || !(coveringNode.getParent() instanceof MethodDeclaration)) {
+			return false;
+		}
+		MethodDeclaration methodDeclaration= (MethodDeclaration)coveringNode.getParent();
+		if (methodDeclaration.getName() != coveringNode || methodDeclaration.resolveBinding() == null) {
+			return false;
+		}
+		if (proposals == null) {
+			return true;
+		}
+
+		final ICompilationUnit cu= context.getCompilationUnit();
+		CompilationUnit cuASTNode= (CompilationUnit)methodDeclaration.getRoot();
+		ASTNode typeDeclarationASTNode= methodDeclaration.getParent();
+		ASTRewrite rewrite= ASTRewrite.create(typeDeclarationASTNode.getAST());
+		String label= "Move to immediate superclass";
+		Image image= JavaPluginImages.get(JavaPluginImages.IMG_MISC_PUBLIC);
+		ASTRewriteCorrectionProposal proposal= new ASTRewriteCorrectionProposal(label, cu, rewrite, 0, image);
+		ASTNode placeHolderForMethodDeclaration= rewrite.createMoveTarget(methodDeclaration);
+		SimpleType superTypeASTNode= (SimpleType)typeDeclarationASTNode.getStructuralProperty(TypeDeclaration.SUPERCLASS_TYPE_PROPERTY);
+		ITypeBinding superTypeBinding= superTypeASTNode.resolveBinding();
+		// See http://wiki.eclipse.org/JDT/FAQ#From_an_IBinding_to_its_declaring_ASTNode
+		ASTNode SuperTypeDeclarationASTNode= cuASTNode.findDeclaringNode(superTypeBinding);
+		ListRewrite superTypeMembersListRewrite= rewrite.getListRewrite(SuperTypeDeclarationASTNode, TypeDeclaration.BODY_DECLARATIONS_PROPERTY);
+		superTypeMembersListRewrite.insertFirst(placeHolderForMethodDeclaration, null);
+		rewrite.remove(methodDeclaration, null);
 		proposals.add(proposal);
 		return true;
 	}
