07edbeb566c0f61c50dfd6d3dc141b380dbfbc88
Mohsen Vakilian <reprogrammer@gmail.com>
2012-07-06 17:47:28 -0500

    Removed the code duplication due to Move to immediate superclass.
    This commit belongs to issue #7.

--- edu.illinois.compositerefactorings.steps/src/edu/illinois/compositerefactorings/steps/CompositeRefactoringsQuickAssistProcessor.java
+++ edu.illinois.compositerefactorings.steps/src/edu/illinois/compositerefactorings/steps/CompositeRefactoringsQuickAssistProcessor.java
@@ -10,46 +10,24 @@ package edu.illinois.compositerefactorings.steps;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jdt.core.ICompilationUnit;
-import org.eclipse.jdt.core.IField;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.IMember;
-import org.eclipse.jdt.core.IMethod;
-import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.BodyDeclaration;
 import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.FieldDeclaration;
-import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
-import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
 import org.eclipse.jdt.core.dom.rewrite.ListRewrite;
-import org.eclipse.jdt.internal.corext.refactoring.JavaRefactoringArguments;
-import org.eclipse.jdt.internal.corext.refactoring.JavaRefactoringDescriptorUtil;
-import org.eclipse.jdt.internal.corext.refactoring.structure.PullUpRefactoringProcessor;
 import org.eclipse.jdt.internal.ui.JavaPluginImages;
 import org.eclipse.jdt.ui.text.java.IInvocationContext;
 import org.eclipse.jdt.ui.text.java.IJavaCompletionProposal;
 import org.eclipse.jdt.ui.text.java.IProblemLocation;
 import org.eclipse.jdt.ui.text.java.IQuickAssistProcessor;
 import org.eclipse.jdt.ui.text.java.correction.ASTRewriteCorrectionProposal;
-import org.eclipse.jdt.ui.text.java.correction.ChangeCorrectionProposal;
 import org.eclipse.jdt.ui.text.java.correction.ICommandAccess;
-import org.eclipse.ltk.core.refactoring.Change;
-import org.eclipse.ltk.core.refactoring.Refactoring;
-import org.eclipse.ltk.core.refactoring.RefactoringStatus;
-import org.eclipse.ltk.core.refactoring.TextFileChange;
-import org.eclipse.ltk.core.refactoring.participants.ProcessorBasedRefactoring;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.text.edits.InsertEdit;
 
 
 @SuppressWarnings("restriction")
@@ -60,7 +38,7 @@ public class CompositeRefactoringsQuickAssistProcessor implements IQuickAssistPr
 		ASTNode coveringNode= context.getCoveringNode();
 		if (coveringNode != null) {
 			return getCreateNewSuperclassProposal(context, coveringNode, false, null) ||
-					getMoveToImmediateSuperclassProposal(context, coveringNode, false, null) ||
+					new MoveToImmediateSuperclass(context, coveringNode, false).hasInputs() ||
 					new MoveTypeToNewFile(context, coveringNode, false).hasInputs() ||
 					new ReplaceTypeBySupertypeInVariableDeclarations(context, coveringNode, false).hasInputs() ||
 					new ReplaceTypeBySupertypeInInstanceOf(context, coveringNode, false).hasInputs();
@@ -74,7 +52,7 @@ public class CompositeRefactoringsQuickAssistProcessor implements IQuickAssistPr
 		if (coveringNode != null) {
 			ArrayList<ICommandAccess> resultingCollections= new ArrayList<ICommandAccess>();
 			getCreateNewSuperclassProposal(context, coveringNode, false, resultingCollections);
-			getMoveToImmediateSuperclassProposal(context, coveringNode, false, resultingCollections);
+			resultingCollections.addAll(new MoveToImmediateSuperclass(context, coveringNode, false).getProposals());
 			resultingCollections.addAll(new MoveTypeToNewFile(context, coveringNode, false).getProposals());
 			resultingCollections.addAll(new ReplaceTypeBySupertypeInVariableDeclarations(context, coveringNode, false).getProposals());
 			resultingCollections.addAll(new ReplaceTypeBySupertypeInInstanceOf(context, coveringNode, false).getProposals());
@@ -114,117 +92,6 @@ public class CompositeRefactoringsQuickAssistProcessor implements IQuickAssistPr
 		rewrite.set(newSuperclass, TypeDeclaration.SUPERCLASS_TYPE_PROPERTY, typeDeclaration.getStructuralProperty(TypeDeclaration.SUPERCLASS_TYPE_PROPERTY), null);
 		rewrite.set(typeDeclaration, TypeDeclaration.SUPERCLASS_TYPE_PROPERTY, newSuperclass.getName(), null);
 		proposals.add(proposal);
-		return true;
-	}
-
-	private static boolean getMoveToImmediateSuperclassProposal(IInvocationContext context, ASTNode coveringNode, boolean problemsAtLocation, Collection<ICommandAccess> proposals)
-			throws CoreException {
-		if (proposals == null) {
-			return true;
-		}
-
-		IMember member= null;
-
-		IMember fieldMember= null;
-
-		FieldDeclaration fieldDeclaration= null;
-
-		if (context.getCoveredNode() instanceof FieldDeclaration) {
-			fieldDeclaration= (FieldDeclaration)context.getCoveredNode();
-		} else if (coveringNode instanceof FieldDeclaration) {
-			fieldDeclaration= (FieldDeclaration)coveringNode;
-		} else if (coveringNode.getParent() instanceof FieldDeclaration) {
-			fieldDeclaration= (FieldDeclaration)coveringNode.getParent();
-		} else if (coveringNode.getParent() != null && coveringNode.getParent().getParent() instanceof FieldDeclaration) {
-			fieldDeclaration= (FieldDeclaration)coveringNode.getParent().getParent();
-		}
-
-		if (fieldDeclaration != null) {
-			// See http://stackoverflow.com/a/11210998/130224
-			VariableDeclarationFragment variableDeclarationFragment= (VariableDeclarationFragment)fieldDeclaration.fragments().get(0);
-			fieldMember= (IMember)variableDeclarationFragment.resolveBinding().getJavaElement();
-		}
-
-		IMember methodMember= null;
-
-		MethodDeclaration methodDeclaration= null;
-
-		if (context.getCoveredNode() instanceof MethodDeclaration) {
-			methodDeclaration= (MethodDeclaration)context.getCoveredNode();
-		} else if (coveringNode instanceof MethodDeclaration) {
-			methodDeclaration= (MethodDeclaration)coveringNode;
-		} else if (coveringNode.getParent() instanceof MethodDeclaration) {
-			methodDeclaration= (MethodDeclaration)coveringNode.getParent();
-		}
-
-		if (methodDeclaration != null) {
-			methodMember= (IMember)methodDeclaration.resolveBinding().getJavaElement();
-		}
-
-		if (fieldMember != null) {
-			member= fieldMember;
-		}
-
-		if (methodMember != null) {
-			member= methodMember;
-		}
-
-		if (member == null) {
-			return false;
-		}
-
-		final ICompilationUnit cu= context.getCompilationUnit();
-		IJavaProject project= cu.getJavaProject();
-
-		IType declaringType= member.getDeclaringType();
-		if (declaringType == null) {
-			return false;
-		}
-		IType immediateSuperclass= declaringType.newTypeHierarchy(project, new NullProgressMonitor()).getSuperclass(declaringType);
-
-		String projectName= project.getElementName();
-		Map<String, String> refactoringArgumentsMap= new HashMap<String, String>();
-		refactoringArgumentsMap.put(JavaRefactoringDescriptorUtil.ATTRIBUTE_INPUT, JavaRefactoringDescriptorUtil.elementToHandle(projectName, immediateSuperclass));
-		refactoringArgumentsMap.put("stubs", String.valueOf(false));
-		refactoringArgumentsMap.put("instanceof", String.valueOf(false));
-		refactoringArgumentsMap.put("replace", String.valueOf(false));
-		refactoringArgumentsMap.put("abstract", String.valueOf(0));
-		int numberOfMethodsToRemoveFromSubclasses= 0;
-		if (member instanceof IField) {
-			numberOfMethodsToRemoveFromSubclasses= 0;
-		} else if (member instanceof IMethod) {
-			numberOfMethodsToRemoveFromSubclasses= 1;
-		}
-		refactoringArgumentsMap.put("delete", Integer.toString(numberOfMethodsToRemoveFromSubclasses));
-		refactoringArgumentsMap.put("pull", String.valueOf(1));
-		refactoringArgumentsMap.put(JavaRefactoringDescriptorUtil.ATTRIBUTE_ELEMENT + 1, JavaRefactoringDescriptorUtil.elementToHandle(projectName, member));
-		if (numberOfMethodsToRemoveFromSubclasses > 0) {
-			refactoringArgumentsMap.put(JavaRefactoringDescriptorUtil.ATTRIBUTE_ELEMENT + 2, JavaRefactoringDescriptorUtil.elementToHandle(projectName, member));
-		}
-
-		JavaRefactoringArguments refactoringArguments= new JavaRefactoringArguments(projectName, refactoringArgumentsMap);
-		PullUpRefactoringProcessor processor= new PullUpRefactoringProcessor(refactoringArguments, new RefactoringStatus());
-		Refactoring refactoring= new ProcessorBasedRefactoring(processor);
-
-		if (refactoring.checkInitialConditions(new NullProgressMonitor()).isOK()) {
-			String label= String.format("Move '%s' to super type '%s'", member.getElementName(), immediateSuperclass.getElementName());
-
-			Image image= JavaPluginImages.get(JavaPluginImages.IMG_MISC_PUBLIC);
-			int relevance= problemsAtLocation ? 1 : 4;
-			RefactoringStatus status= refactoring.checkFinalConditions(new NullProgressMonitor());
-			Change change= null;
-			if (status.hasFatalError()) {
-				change= new TextFileChange("fatal error", (IFile)cu.getResource()); //$NON-NLS-1$
-				((TextFileChange)change).setEdit(new InsertEdit(0, "")); //$NON-NLS-1$
-				return false;
-			} else {
-				change= refactoring.createChange(new NullProgressMonitor());
-			}
-			ChangeCorrectionProposal proposal= new ChangeCorrectionProposal(label, change, relevance, image);
-
-			proposals.add(proposal);
-		}
-
 		return true;
 	}
 
---
+++ edu.illinois.compositerefactorings.steps/src/edu/illinois/compositerefactorings/steps/MoveToImmediateSuperclass.java
@@ -0,0 +1,126 @@
+/**
+ * Copyright (c) 2008-2012 University of Illinois at Urbana-Champaign.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package edu.illinois.compositerefactorings.steps;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jdt.core.IField;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IMember;
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.FieldDeclaration;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
+import org.eclipse.jdt.core.refactoring.descriptors.PullUpDescriptor;
+import org.eclipse.jdt.internal.corext.refactoring.JavaRefactoringDescriptorUtil;
+import org.eclipse.jdt.ui.text.java.IInvocationContext;
+import org.eclipse.ltk.core.refactoring.RefactoringDescriptor;
+
+@SuppressWarnings("restriction")
+public class MoveToImmediateSuperclass extends RefactoringBasedStep {
+
+	public MoveToImmediateSuperclass(IInvocationContext context, ASTNode coveringNode, boolean problemsAtLocation) {
+		super(context, coveringNode, problemsAtLocation);
+	}
+
+	@Override
+	protected Collection<? extends IJavaElement> getInputs() {
+		Collection<IMember> members= new ArrayList<IMember>();
+		IMember member= null;
+
+		IMember fieldMember= null;
+
+		FieldDeclaration fieldDeclaration= null;
+
+		if (context.getCoveredNode() instanceof FieldDeclaration) {
+			fieldDeclaration= (FieldDeclaration)context.getCoveredNode();
+		} else if (coveringNode instanceof FieldDeclaration) {
+			fieldDeclaration= (FieldDeclaration)coveringNode;
+		} else if (coveringNode.getParent() instanceof FieldDeclaration) {
+			fieldDeclaration= (FieldDeclaration)coveringNode.getParent();
+		} else if (coveringNode.getParent() != null && coveringNode.getParent().getParent() instanceof FieldDeclaration) {
+			fieldDeclaration= (FieldDeclaration)coveringNode.getParent().getParent();
+		}
+
+		if (fieldDeclaration != null) {
+			// See http://stackoverflow.com/a/11210998/130224
+			VariableDeclarationFragment variableDeclarationFragment= (VariableDeclarationFragment)fieldDeclaration.fragments().get(0);
+			fieldMember= (IMember)variableDeclarationFragment.resolveBinding().getJavaElement();
+		}
+
+		IMember methodMember= null;
+
+		MethodDeclaration methodDeclaration= null;
+
+		if (context.getCoveredNode() instanceof MethodDeclaration) {
+			methodDeclaration= (MethodDeclaration)context.getCoveredNode();
+		} else if (coveringNode instanceof MethodDeclaration) {
+			methodDeclaration= (MethodDeclaration)coveringNode;
+		} else if (coveringNode.getParent() instanceof MethodDeclaration) {
+			methodDeclaration= (MethodDeclaration)coveringNode.getParent();
+		}
+
+		if (methodDeclaration != null) {
+			methodMember= (IMember)methodDeclaration.resolveBinding().getJavaElement();
+		}
+
+		if (fieldMember != null) {
+			member= fieldMember;
+		}
+
+		if (methodMember != null) {
+			member= methodMember;
+		}
+
+		if (member != null && member.getDeclaringType() != null) {
+			members.add(member);
+		}
+
+		return members;
+	}
+
+	@Override
+	protected Collection<RefactoringDescriptor> getDescriptors(IJavaElement input) throws CoreException {
+		Collection<RefactoringDescriptor> descriptors= new ArrayList<RefactoringDescriptor>();
+		IMember member= (IMember)input;
+		IType declaringType= member.getDeclaringType();
+		IType immediateSuperclass= declaringType.newTypeHierarchy(getJavaProject(), new NullProgressMonitor()).getSuperclass(declaringType);
+
+		Map<String, String> arguments= new HashMap<String, String>();
+		arguments.put(JavaRefactoringDescriptorUtil.ATTRIBUTE_INPUT, JavaRefactoringDescriptorUtil.elementToHandle(getJavaProject().getElementName(), immediateSuperclass));
+		arguments.put("stubs", String.valueOf(false));
+		arguments.put("instanceof", String.valueOf(false));
+		arguments.put("replace", String.valueOf(false));
+		arguments.put("abstract", String.valueOf(0));
+		int numberOfMethodsToRemoveFromSubclasses= 0;
+		if (member instanceof IField) {
+			numberOfMethodsToRemoveFromSubclasses= 0;
+		} else if (member instanceof IMethod) {
+			numberOfMethodsToRemoveFromSubclasses= 1;
+		}
+		arguments.put("delete", Integer.toString(numberOfMethodsToRemoveFromSubclasses));
+		arguments.put("pull", String.valueOf(1));
+		arguments.put(JavaRefactoringDescriptorUtil.ATTRIBUTE_ELEMENT + 1, JavaRefactoringDescriptorUtil.elementToHandle(getJavaProject().getElementName(), member));
+		if (numberOfMethodsToRemoveFromSubclasses > 0) {
+			arguments.put(JavaRefactoringDescriptorUtil.ATTRIBUTE_ELEMENT + 2, JavaRefactoringDescriptorUtil.elementToHandle(getJavaProject().getElementName(), member));
+		}
+		String description= String.format("Move '%s' to super type '%s'", member.getElementName(), immediateSuperclass.getElementName());
+		PullUpDescriptor descriptor= new PullUpDescriptor(getJavaProject().getElementName(), description, null, arguments, RefactoringDescriptor.STRUCTURAL_CHANGE | RefactoringDescriptor.MULTI_CHANGE);
+		descriptors.add(descriptor);
+		return descriptors;
+	}
+
+}
