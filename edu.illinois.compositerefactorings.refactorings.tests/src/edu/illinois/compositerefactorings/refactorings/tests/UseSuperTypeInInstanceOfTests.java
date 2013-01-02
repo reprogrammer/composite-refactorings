297896b45dbbdddcb2399e2cb35c2998ce9893d9
Mohsen Vakilian <reprogrammer@gmail.com>
2013-01-02 13:29:04 -0600

    Removed some duplication in test code.
    This commit belongs to issue #20.

--- edu.illinois.compositerefactorings.refactorings.tests/src/edu/illinois/compositerefactorings/refactorings/tests/UseSuperTypeInInstanceOfTests.java
+++ edu.illinois.compositerefactorings.refactorings.tests/src/edu/illinois/compositerefactorings/refactorings/tests/UseSuperTypeInInstanceOfTests.java
@@ -8,25 +8,19 @@
 
 package edu.illinois.compositerefactorings.refactorings.tests;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.IType;
-import org.eclipse.jdt.internal.corext.codemanipulation.StubUtility;
-import org.eclipse.jdt.internal.corext.template.java.CodeTemplateContextType;
-import org.eclipse.jdt.ui.tests.refactoring.RefactoringTest;
 import org.eclipse.jdt.ui.tests.refactoring.RefactoringTestSetup;
-import org.eclipse.ltk.core.refactoring.Refactoring;
-import org.eclipse.ltk.core.refactoring.RefactoringStatus;
 
 import edu.illinois.compositerefactorings.refactorings.usesupertypeininstanceof.UseSuperTypeInInstanceOfDescriptor;
 
@@ -53,78 +47,35 @@ public class UseSuperTypeInInstanceOfTests extends RefactoringTest {
 		return REFACTORING_PATH;
 	}
 
-	protected void setUp() throws Exception {
-		super.setUp();
-		StubUtility.setCodeTemplate(CodeTemplateContextType.NEWTYPE_ID,
-				"${package_declaration}" +
-						System.getProperty("line.separator", "\n") +
-						"${" + CodeTemplateContextType.TYPE_COMMENT + "}" +
-						System.getProperty("line.separator", "\n") +
-						"${type_declaration}", null);
+	private void validatePassingTest(String className, List<String> cuNames, String superTypeFullName, boolean replaceInstanceOf) throws Exception {
+		final Map<String, ICompilationUnit> units= new HashMap<String, ICompilationUnit>();
+		IType subtype= getClassFromTestFile(getPackageP(), className);
+		units.put(className, subtype.getCompilationUnit());
+		IPackageFragment packageFragment= subtype.getPackageFragment();
+		units.putAll(getCompilationUnits(packageFragment, cuNames));
 
-		StubUtility.setCodeTemplate(CodeTemplateContextType.TYPECOMMENT_ID, "/** typecomment template*/", null);
-	}
-
-	@Override
-	public String getFileContents(String fileName) throws IOException {
-		return getContents(getFileInputStream(fileName));
-	}
-
-	public static InputStream getFileInputStream(String fileName) throws IOException {
-		IPath path= new Path("resources").append(fileName);
-		try {
-			return Activator.getDefault().getFileInPlugin(path).toURI().toURL().openStream();
-		} catch (CoreException e) {
-			throw new IOException(e);
-		}
-	}
-
-	private IType getClassFromTestFile(IPackageFragment pack, String className) throws Exception {
-		return getType(createCUfromTestFile(pack, className), className);
-	}
-
-	private void validatePassingTest(String className, String[] cuNames, String superTypeFullName, boolean replaceInstanceOf) throws Exception {
-		final IType subType= getClassFromTestFile(getPackageP(), className);
-		final ICompilationUnit[] units= new ICompilationUnit[cuNames.length];
-		for (int i= 0; i < cuNames.length; i++) {
-			if (cuNames[i].equals(subType.getCompilationUnit().findPrimaryType().getElementName()))
-				units[i]= subType.getCompilationUnit();
-			else
-				units[i]= createCUfromTestFile(subType.getPackageFragment(), cuNames[i]);
-
-		}
-		final IType superType= subType.getJavaProject().findType(superTypeFullName, (IProgressMonitor)null);
+		final IType superType= subtype.getJavaProject().findType(superTypeFullName, (IProgressMonitor)null);
 		final UseSuperTypeInInstanceOfDescriptor descriptor= new UseSuperTypeInInstanceOfDescriptor();
-		descriptor.setSubtype(subType);
+		descriptor.setSubtype(subtype);
 		descriptor.setSupertype(superType);
-		final RefactoringStatus status= new RefactoringStatus();
-		final Refactoring refactoring= descriptor.createRefactoring(status);
-		assertTrue("status should be ok", status.isOK());
-		assertNotNull("refactoring should not be null", refactoring);
-		assertEquals("was supposed to pass", null, performRefactoring(refactoring));
-
-		for (int i= 0; i < units.length; i++) {
-			String expected= getFileContents(getOutputTestFileName(cuNames[i]));
-			String actual= units[i].getSource();
-			String message= "incorrect changes in " + units[i].getElementName();
-			assertEqualLines(message, expected, actual);
-		}
+		createPerformCheckRefactoring(descriptor);
+		compareCompilationUnits(units);
 	}
 
-	private void validatePassingTest(String className, String[] cuNames, String superTypeFullName) throws Exception {
+	private void validatePassingTest(String className, List<String> cuNames, String superTypeFullName) throws Exception {
 		validatePassingTest(className, cuNames, superTypeFullName, false);
 	}
 
 	public void test0() throws Exception {
-		validatePassingTest("C", new String[] { "C" }, "p.I");
+		validatePassingTest("C", Arrays.<String> asList(), "p.I");
 	}
 
 	public void test1() throws Exception {
-		validatePassingTest("C", new String[] { "C", "I" }, "p.I");
+		validatePassingTest("C", Arrays.asList("I"), "p.I");
 	}
 
 	public void test2() throws Exception {
-		validatePassingTest("C", new String[] { "C", "D" }, "p.D");
+		validatePassingTest("C", Arrays.asList("D"), "p.D");
 	}
 
 }
